20cba3527edc29d222080bd9c1693bb5ae4ce331
Connor Kennedy <cjtkennedy@gmail.com>
2016-04-13 23:08:26 -0500

    Allow a single pug

--- scripts/pugme.js
+++ scripts/pugme.js
@@ -1,7 +1,10 @@
 module.exports = function(robot){
 
 	robot.respond(/pug me/i, function(res){
-		res.reply('There will be no more of that.');
+		res.http('http://pugme.herokuapp.com/random')
+		   .get(function(err, res, body){
+		   		res.send(JSON.parse(body).pug);
+		   });
 	});
 	robot.respond(/pug bomb (.*)/i, function(res){
 		var number = res.match[1];
