f466b4bd4abb5c30e4a24f3a9edbbd7955301fdb
Connor Kennedy <cjtkennedy@gmail.com>
2016-04-14 17:47:54 -0500

    Add underscorejs and replace rules module with custom code

---
+++ scripts/rules.js
@@ -0,0 +1,13 @@
+module.exports = function(robot){
+
+	var rules = [
+		'1. A robot may not harm humans or humanity in general, or, by inaction, allow humans or humanity as a whole to come to harm.',
+		'2. A robot must obey any orders given to it by human beings, except where such orders would conflict with the First Law.',
+		'3. A robot must protect its own existence as long as such protection does not conflict with the First or Second Law.'
+	];
+
+	robot.respond(/rules/i, function(res){
+		res.send(rules.join('\n'));
+	});
+
+}
