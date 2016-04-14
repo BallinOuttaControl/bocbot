e6492b5a5f6f904a6cf24015ef0d710f6b3b5532
Connor Kennedy <cjtkennedy@gmail.com>
2016-04-13 23:01:37 -0500

    Fix pug bomb command

--- scripts/pugme.js
+++ scripts/pugme.js
@@ -3,7 +3,7 @@ module.exports = function(robot){
 	robot.respond(/pug me/i, function(res){
 		res.reply('There will be no more of that.');
 	});
-	robot.respond(/pug me (.*)/i, function(res){
+	robot.respond(/pug bomb (.*)/i, function(res){
 		var number = res.match[1];
 		res.reply('There will be no more of that.  Take your ' + number.trim() + ' pugs elsewhere.');
 	});
