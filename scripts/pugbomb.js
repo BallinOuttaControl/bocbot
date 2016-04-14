6a51198e41261fdcdc917ca32452184d8c29b1d4
Connor Kennedy <cjtkennedy@gmail.com>
2016-04-14 00:02:40 -0500

    Randomize pug bomb deflections

--- scripts/pugbomb.js
+++ scripts/pugbomb.js
@@ -1,8 +1,19 @@
 module.exports = function(robot){
 
+	var pugBombReplies = [
+		'There will be no more of that.  Take your {{number}} pugs elsewhere.',
+		'Nope.  Nobody needs to see that many pugs.',
+		'Why do you think we all need to see {{number}} pugs?',
+		'No pugs for you!',
+		'{{number}} pugs... {{number}} PUGS??  Absolutely not.'
+	];
+
 	robot.respond(/pug bomb (.*)/i, function(res){
-		var number = res.match[1];
-		res.reply('There will be no more of that.  Take your ' + number.trim() + ' pugs elsewhere.');
+		var number = res.match[1],
+			index = Math.floor(Math.random() * pugBombReplies.length),
+			reply = pugBombReplies[index].replace(/{{number}}/g, number.trim());
+
+		res.reply(reply);
 	});
 
 }
