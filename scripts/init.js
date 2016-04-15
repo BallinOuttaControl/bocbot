c1eded40a539edd47aacce2e877c50596be6b5a0
Connor Kennedy <cjtkennedy@gmail.com>
2016-04-14 21:00:06 -0500

    Correct test function

--- scripts/init.js
+++ scripts/init.js
@@ -3,14 +3,14 @@ module.exports = function(robot){
 	robot.brain.set('testmemory', 'memory remembered');
 
 	robot.respond(/memorytest/i, function(res){
-		res.respond(robot.brain.get('testmemory'));
+		res.reply(robot.brain.get('testmemory'));
 	});
 
 	robot.respond(/memorytest (.*)/i, function(res){
 		var newMemory = res.match[1];
 		var oldMemory = robot.brain.get('testmemory');
 		robot.brain.set('memorytest', newMemory);
-		res.respond('Memory changed from ' + oldMemory + ' to ' + newMemory);
+		res.reply('Memory changed from ' + oldMemory + ' to ' + newMemory);
 	});
 
 }
