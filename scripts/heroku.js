3e1537c3bd88340b81f5aba5792cbe2470041fbd
Connor Kennedy <cjtkennedy@gmail.com>
2016-04-16 21:02:04 -0500

    More heroku testing

--- scripts/heroku.js
+++ scripts/heroku.js
@@ -3,10 +3,16 @@ var Heroku = require('heroku-client'),
 
 module.exports = function(robot){
 
-	robot.respond(/list heroku apps/i, function(res){
+	robot.respond(/list bocbot-dev info/i, function(res){
 		var app = heroku.apps('bocbot-dev');
 		app.info(function(err, app){
 			res.reply(JSON.stringify(app, null, '\t'));
+		});
+	});
+
+	robot.respond(/list heroku apps/i, function(res){
+		heroku.apps().list(function(err, apps){
+			res.reply(JSON.stringify(apps));
 		});
 	});
 
