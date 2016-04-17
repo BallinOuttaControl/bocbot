1c95e04f7c9384d4c7afbbfa34052ec3ede3be51
Connor Kennedy <cjtkennedy@gmail.com>
2016-04-16 22:01:02 -0500

    Beginnings of Heroku api control

--- scripts/heroku.js
+++ scripts/heroku.js
@@ -3,16 +3,35 @@ var Heroku = require('heroku-client'),
 
 module.exports = function(robot){
 
-	robot.respond(/list bocbot-dev info/i, function(res){
-		var app = heroku.apps('bocbot-dev');
-		app.info(function(err, app){
-			res.reply(JSON.stringify(app, null, '\t'));
+	robot.respond(/list heroku apps/i, function(res){
+		heroku.apps().list(function(err, apps){
+			res.send(JSON.stringify(apps, null, '\t'));
 		});
 	});
 
-	robot.respond(/list heroku apps/i, function(res){
-		heroku.apps().list(function(err, apps){
-			res.reply(JSON.stringify(apps, null, '\t'));
+	robot.respond(/promote staging to production/i, function(res){
+		heroku.post('/pipeline-promotions', {
+			pipeline: {
+				id: process.env.HEROKU_PIPELINE_ID
+			},
+			source: {
+				app: {
+					id: process.env.HEROKU_SOURCE_APP_ID
+				}
+			},
+			targets: [
+				{
+					app: {
+						id: process.env.HEROKU_TARGET_APP_ID
+					}
+				}
+			]
+		},
+		function(err, app){
+			if(!!err)
+				res.reply('There was an error processing your request');
+			else
+				res.reply('Promotion initiated');
 		});
 	});
 
