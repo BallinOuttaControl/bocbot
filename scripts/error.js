f3996ea80cbfc9160f91419a2e402d9341f4c746
Connor Kennedy <cjtkennedy@gmail.com>
2016-04-18 16:34:36 -0500

    Update error logging

--- scripts/error.js
+++ scripts/error.js
@@ -1,7 +1,7 @@
 module.exports = function(robot){
 	
 	robot.error(function(err, res){
-		robot.logger.error("Does not compute");
+		robot.logger.error(err);
 
 		if (!!res)
 			res.reply('Does not compute!');
