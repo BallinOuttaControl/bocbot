fe8b3e7588a667c83d2a1f4434bcff484caa296a
Connor Kennedy <cjtkennedy@gmail.com>
2016-04-28 01:27:14 -0500

    Add error image

--- scripts/error.js
+++ scripts/error.js
@@ -1,10 +1,12 @@
 module.exports = function(robot){
+
+	var errorImageUrl = 'http://i.imgur.com/1QXZmWG.jpg';
 	
 	robot.error(function(err, res){
 		robot.logger.error(err);
 
 		if (!!res)
-			res.reply('Does not compute!');
+			res.reply('Does not compute!\n' + errorImageUrl);
 	});
 
 }
