d531e2e25b4919a862388135766d5efbb7782428
Connor Kennedy <cjtkennedy@gmail.com>
2016-05-25 22:59:47 -0500

    Add 'this day in history' facts

--- scripts/facts.js
+++ scripts/facts.js
@@ -1,12 +1,17 @@
+// Commands:
+//   bocbot [give me a ][random ] fact - Gets a random fact
+//   bocbot this day in history - Gets a random historical fact about today's date
+//   bocbot this day in history <date> - Gets a random historical fact about the specified date
+
 module.exports = function(robot){
 
 	robot.facts = {
 		factApiUrl: 'http://numbersapi.com/random',
+		dateFactApiUrl: 'http://numbersapi.com/{{month}}/{{day}}/date',
 		factHashTag: '#bocbotfacts',
 
 		sendFact: function(response){
-			var self = this,
-				fact = '';
+			var self = this;
 			robot.http(this.factApiUrl).get()(function(err, res, body){
 				if (!!err){ // Error processing request
 					robot.error.log(err);
@@ -17,10 +22,39 @@ module.exports = function(robot){
 
 				response.send(body + '  ' + self.factHashTag);
 			});
+		},
+
+		sendDateFact: function(response, date){
+			var self = this,
+				date = !!date ? new Date(date) : new Date(),  // If a date was not passed in, use today's date
+				month = date.getMonth() + 1,
+				day = date.getDate(),
+				url = this.dateFactApiUrl.replace(/{{month}}/, month).replace(/{{day}}/, day);
+			robot.http(url).get()(function(err, res, body){
+				if (!!err){
+					robot.error.log(err);
+					return;
+				}
+
+				response.send(body);
+			});
 		}
 	};
 
 	robot.hear(/#(.*)fact/i, function(res){
 		robot.facts.sendFact(res);
+	});
+
+	robot.respond(/(give me a )*(random )*fact/i, function(res){
+		robot.facts.sendFact(res);
+	});
+
+	robot.respond(/this day in history(.*)/i, function(res){
+		if (!!res.match[1]){
+			var date = res.match[1].trim();
+			robot.facts.sendDateFact(res, date);
+		}
+		else
+			robot.facts.sendDateFact(res);
 	});
 }
