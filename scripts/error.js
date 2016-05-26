70ffda1fd04066791b5690130d69cf2353407d1b
Connor Kennedy <cjtkennedy@gmail.com>
2016-05-25 20:59:36 -0500

    Implement some useful error logging

--- scripts/error.js
+++ scripts/error.js
@@ -3,21 +3,21 @@ module.exports = function(robot){
 	robot.errors = {
 		imageUrl: 'http://i.imgur.com/1QXZmWG.jpg',
 		logChannel: 'bocbot-errors',
-		errorMessage: 'ERROR while processing a request for {{user}} in {{room}}.  {{stacktrace}}',
 
 		createError: function(err, res){
-			var errorMsg = this.errorMessage.replace(/{{user}}/g, res.message.user.name)
-											.replace(/{{room}}/g, res.message.room)
-											.replace(/{{stacktrace}}/g, err.stack);
-			return errorMsg;
+			var stackTraceLines = err.stack.split('\n');
+			stackTraceLines[0] = '*' + stackTraceLines[0] + '*';
+			return stackTraceLines.join('\n');
 		}
 	};
 	
+	// Set error callback
 	robot.error(function(err, res){
 		robot.logger.error(err);
-		robot.messageRoom(robot.errors.logChannel, "ERROR");
+		robot.messageRoom(robot.errors.logChannel, robot.errors.createError(err));
 	});
 
+	// Make a nonsense error for testing purposes
 	robot.respond(/generate error/i, function(res){
 		res.send(robot.somecrazyvaluethatisalwaysgoingtoremaindundefinedunlessbyblindlucksomeonesetsthistosomethingotherthanundefined);
 	});
