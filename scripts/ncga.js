8f488c44e3709dc16f75ddc25fc79191e365c78b
Connor Kennedy <Conroman16@users.noreply.github.com>
2020-03-10 01:28:35 -0500

    Tidy up

--- scripts/ncga.js
+++ scripts/ncga.js
@@ -5,13 +5,13 @@ let ncga = require('../lib/ncga');
 
 module.exports = function(robot){
 
-	robot.respond(/soil ?temps?(?: (\S+))?$/i, (res) => {
+	robot.respond(/soil ?temps?(?: (\S+))?$/i, async (res) => {
 		let mapID = ncga.maps[res.match[1] || 'us'];
 		if (!mapID)
 			return res.reply(`I'm not aware of any map with an ID of \`${res.match[1]}\``);
 
-		ncga.urls.imageUrl(mapID)
-			.then(url => res.reply(`<${url}|${mapID}>`))
-			.catch(e => res.reply(`ERROR: ${e.message || e}`));
+		let url = await ncga.urls.imageUrl(mapID);
+		return res.reply(`<${url}|${mapID}>`);
+
 	});
 }
